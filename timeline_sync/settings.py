dd8da3f82e662f206bc996d8b1162b68debe8e20
Joshua Wise <joshua@accelerated.tech>
2019-07-12 22:10:11 -0400

    You have to say the magic word.

--- timeline_sync/settings.py
+++ timeline_sync/settings.py
@@ -8,4 +8,5 @@ config = {
     'DOMAIN_ROOT': domain_root,
     'REBBLE_AUTH_URL': environ.get('REBBLE_AUTH_URL', f"{http_protocol}://auth.{domain_root}"),
     'APPSTORE_API_URL': environ.get('APPSTORE_API_URL', f"{http_protocol}://appstore-api.{domain_root}"),
+    'SECRET_KEY': environ.get('SECRET_KEY'),
 }
