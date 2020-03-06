d2f73652e13a354e48683856a111cee4295f450e
Joshua Wise <joshua@accelerated.tech>
2020-03-06 14:19:45 -0500

    Honeycombize.

--- timeline_sync/__init__.py
+++ timeline_sync/__init__.py
@@ -4,8 +4,14 @@ from .settings import config
 from .api import init_api
 from .models import init_app, delete_expired_pins
 
+import beeline
+from beeline.patch import requests
+
 app = Flask(__name__)
 app.config.update(**config)
+if config['HONEYCOMB_KEY']:
+     beeline.init(writekey=config['HONEYCOMB_KEY'], dataset='rws', service_name='timeline_sync')
+     HoneyMiddleware(app, db_events = True)
 init_app(app)
 init_api(app)  # Includes both private (timeline-sync) and public (timeline-api) APIs
 
@@ -15,3 +21,52 @@ def heartbeat():
 
 def nightly_maintenance():
     delete_expired_pins(app)
+
+# XXX: This maybe ought be upstreamed to Beeline.
+from wrapt import wrap_function_wrapper
+import beeline
+import urllib.request
+
+def _urllibopen(_urlopen, instance, args, kwargs):
+    if type(args[0]) != urllib.request.Request:
+        args[0] = urllib.request.Request(args[0])
+    
+    span = beeline.start_span(context={"meta.type": "http_client"})
+    
+    b = beeline.get_beeline()
+    if b:
+        context = b.tracer_impl.marshal_trace_context()
+        if context:
+            b.log("urllib lib - adding trace context to outbound request: %s", context)
+            args[0].headers['X-Honeycomb-Trace'] = context
+        else:
+            b.log("urllib lib - no trace context found")
+    
+    try:
+        resp = None
+        beeline.add_context({
+            "name": "urllib_%s" % args[0].get_method(),
+            "request.method": args[0].get_method(),
+            "request.uri": args[0].full_url
+        })
+        resp = _urlopen(*args, **kwargs)
+        return resp
+    except Exception as e:
+        beeline.add_context({
+            "request.error_type": str(type(e)),
+            "request.error": beeline.internal.stringify_exception(e),
+        })
+        raise
+    finally:
+        if resp:
+            beeline.add_context_field("response.status_code", resp.status)
+            content_type = resp.getheader('content-type')
+            if content_type:
+                beeline.add_context_field("response.content_type", content_type)
+            content_length = resp.getheader('content-length')
+            if content_length:
+                beeline.add_context_field("response.content_length", content_length)
+            
+        beeline.finish_span(span)
+
+wrap_function_wrapper('urllib.request', 'urlopen', _urllibopen)
